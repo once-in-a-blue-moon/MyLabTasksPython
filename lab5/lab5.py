761c2a5964abc017a558fce2e9ac847ddf5697de
Lera <vsarkisan62@gmail.com>
2021-11-24 12:48:36 +0200

    lab 5 (edited)

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -1,13 +1,16 @@
 print("enter a, b: ")
 a=int(input())
 b=int(input())
-for n in range(a, b+1, 1):
-    if (n>0):
-        print("n =", n, ". dividers are: ")
-        k=0; S=0
-        for i in range(1, (n+1), 1):
-            if (n % i == 0):
-                k=k+1
-                S=S+i
-                print(i)
-        print("number of dividers:", k, ". Sum of dividers:", S)
+if (a>b):
+    print("b must be > a")
+else:
+    for n in range(a, b+1, 1):
+        if (n>0):
+            print("n =", n, ". dividers are: ")
+            k=0; S=0
+            for i in range(1, (n+1), 1):
+                if (n % i == 0):
+                    k=k+1
+                    S=S+i
+                    print(i)
+            print("number of dividers:", k, ". Sum of dividers:", S)
